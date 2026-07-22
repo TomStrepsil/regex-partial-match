90376551a2ffed8be2e82bf9cf4450775f0f4d8c
Tom Pereira <10725179+TomStrepsil@users.noreply.github.com>
2026-07-22 16:58:28 +0100

    [NO ISSUE] remove named export (#64)
    - remove errant named export

--- src/partialMatchRegExp.test.ts
+++ src/partialMatchRegExp.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { PartialMatchRegExp } from "./partialMatchRegExp.ts";
+import PartialMatchRegExp from "./partialMatchRegExp.ts";
 
 describe("PartialMatchRegExp", () => {
   it("is an instance of RegExp", () => {
