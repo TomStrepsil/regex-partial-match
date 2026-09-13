75aa18c0009af9b951d1340f1dd6b5cdc1df227a
Tom Pereira <10725179+TomStrepsil@users.noreply.github.com>
2026-09-13 22:19:02 +0100

    another part of constants move

--- src/partialMatchRegExp/constants.ts
+++ src/partialMatchRegExp/constants.ts
@@ -1,5 +1,3 @@
-import asOptionalAtom from "./asOptionalAtom.ts";
-
 export const OCCURRENCES_REGEX = /\{\d+,?\d*\}/y;
 export const NOT_NUMBERS_REGEX = /\D/g;
 export const FLAGS_IRRELEVANT_TO_REBUILD = /[dgy]/g;
@@ -10,13 +8,4 @@ export const LOOKAHEAD_OPENING = "(?=";
 export const DISJUNCTION_TO_END_OF_INPUT = "|$(?![\\s\\S]))";
 export const OPTIONAL_ATOM_OPENING = "(?:";
 export const NAMED_GROUP_OPENING = "(?<";
-export const ANY_CAPTURED_TEXT = "(?:[\\s\\S]*?)";
 export const QUANTIFIER_PART = /^(?:[*+?]|\{\d+,?\d*\})$/;
-export const MAYBE_HAS_BACKREFERENCE_REGEX = /\\[0-9]|\\k</;
-export const NEVER = "(?!)";
-export const ALTERNATION = "|";
-export const ONLY_AT_END_OF_INPUT = asOptionalAtom(NEVER);
-export const UNCONSTRAINED_GROUP_SHAPE = {
-  groupLimit: Infinity,
-  declaresNamedGroup: true
-};
